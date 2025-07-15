b8c3f6e3a0ecc273e4e8814b23f9f7e65a4b0d68
Jang Seung Beom <wkdtmf357@gmail.com>
2025-07-15 19:07:14 +0900

    feat: Coupon, UserCoupon 엔티티 추가

---
+++ src/main/java/com/ureka/team3/utong_scheduler/coupon/entity/Coupon.java
@@ -0,0 +1,39 @@
+package com.ureka.team3.utong_scheduler.coupon.entity;
+
+import com.ureka.team3.utong_scheduler.gift.entity.Gifticon;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Coupon {
+
+    @Id
+    @Column(length = 36)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gifticon_id", nullable = false)
+    private Gifticon gifticon;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "expired_at")
+    private LocalDateTime expiredAt;
+
+    @Column(name = "coupon_code", length = 3)
+    private String couponCode; // 공통코드 030 참조
+}
