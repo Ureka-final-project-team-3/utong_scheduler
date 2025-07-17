19bd10c37f7ff92e174da9962184079220bca6dd
Jang Seung Beom <wkdtmf357@gmail.com>
2025-07-17 12:12:03 +0900

    fix: INSERT-SELECT UUID 자동생성 충돌 문제

--- src/main/java/com/ureka/team3/utong_scheduler/contract/entity/ContractHourlyAvgPrice.java
+++ src/main/java/com/ureka/team3/utong_scheduler/contract/entity/ContractHourlyAvgPrice.java
@@ -18,8 +18,6 @@ import java.time.LocalDateTime;
 public class ContractHourlyAvgPrice {
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(length = 36)
     private String id;
 
