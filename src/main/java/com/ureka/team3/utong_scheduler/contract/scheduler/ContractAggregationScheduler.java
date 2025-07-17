d21a04bf7960207f98f12791cb0846afe757b3e2
Jang Seung Beom <wkdtmf357@gmail.com>
2025-07-17 17:09:27 +0900

    fix: 계약 집계 스케줄러 로직 변경   - 최근 1시간 데이터 없는 경우 -> 가장 가까운 집계 데이터 저장   - 아무 거래 데이터 없는 경우 -> 어드민 설정 하한가로 저장

--- src/main/java/com/ureka/team3/utong_scheduler/contract/scheduler/ContractAggregationScheduler.java
+++ src/main/java/com/ureka/team3/utong_scheduler/contract/scheduler/ContractAggregationScheduler.java
@@ -3,6 +3,8 @@ package com.ureka.team3.utong_scheduler.contract.scheduler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ureka.team3.utong_scheduler.contract.dto.AggregationStatusMessage;
 import com.ureka.team3.utong_scheduler.contract.repository.ContractHourlyAvgPriceRepository;
+import com.ureka.team3.utong_scheduler.price.entity.Price;
+import com.ureka.team3.utong_scheduler.price.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -18,10 +20,12 @@ import java.time.LocalDateTime;
 public class ContractAggregationScheduler {
 
     private final ContractHourlyAvgPriceRepository contractHourlyAvgPriceRepository;
+    private final PriceRepository priceRepository;
     private final StringRedisTemplate stringRedisTemplate;
     private final ObjectMapper objectMapper;
 
     private static final String CONTRACT_AGGREGATION_STATUS_CHANNEL = "contract:aggregation:status";
+    private static final String PRICE_ID = "903ee67c-71b3-432e-bbd1-aaf5d5043376";
 
     // 매시간 계약 평균가를 계산하여 저장하는 스케줄러
     @Scheduled(cron = "0 0 * * * *")
@@ -35,10 +39,28 @@ public class ContractAggregationScheduler {
             LocalDateTime currentHour = now.withMinute(0).withSecond(0).withNano(0);
             LocalDateTime previousHour = now.minusHours(1).withMinute(0).withSecond(0).withNano(0);
 
-            // 평균가 계산 및 저장
-            contractHourlyAvgPriceRepository.insertHourlyAvgPrice(previousHour, currentHour);
+            // 해당 시간대 거래 건수 확인
+            int contractCount = contractHourlyAvgPriceRepository.countContractsByTimeRange(previousHour, currentHour);
 
-            log.info("계약 평균가 집계 완료: {} ~ {}", previousHour, currentHour);
+            // 평균가 계산 및 저장
+            if (contractCount > 0) { // 최근 1시간 거래가 있는 경우
+                contractHourlyAvgPriceRepository.insertHourlyAvgPrice(previousHour, currentHour);
+                log.info("계약 평균가 집계 완료: {}건, {} ~ {}", contractCount, previousHour, currentHour);
+            } else { // 최근 1시간 거래가 없는 경우 -> 이전 시간의 평균가 중 가장 가까운 시간의 평균가를 사용
+                Long previousPrice = contractHourlyAvgPriceRepository.findLatestAvgPrice(previousHour);
+
+                if(previousPrice != null) {
+                    contractHourlyAvgPriceRepository.insertHourlyAvgPriceWithValue(previousHour, previousPrice);
+                    log.info("이전 가격 기반 집계 완료: {} 원", previousPrice);
+                }
+                else { // 아무 거래도 없는 경우
+                    Price price = priceRepository.findById(PRICE_ID)
+                            .orElseThrow(() -> new RuntimeException("기본 가격 정보가 없습니다."));
+
+                    contractHourlyAvgPriceRepository.insertHourlyAvgPriceWithValue(previousHour, price.getMinimumPrice());
+                    log.info("기본 가격 기반 집계 완료: {} 원", price.getMinimumPrice());
+                }
+            }
 
             publishAggregationComplete(previousHour);
         }
