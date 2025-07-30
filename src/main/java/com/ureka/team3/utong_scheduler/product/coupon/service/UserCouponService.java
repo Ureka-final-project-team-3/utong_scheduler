3d3405c1b69c8e6e934731035e9abdff025a92b4
Jang Seung Beom <wkdtmf357@gmail.com>
2025-07-30 17:06:04 +0900

    feat: 배치를 통한 만료 스케줄링 처리

---
+++ src/main/java/com/ureka/team3/utong_scheduler/product/coupon/service/UserCouponService.java
@@ -0,0 +1,9 @@
+package com.ureka.team3.utong_scheduler.product.coupon.service;
+
+import com.ureka.team3.utong_scheduler.product.dto.ExpirationResult;
+
+public interface UserCouponService {
+
+    ExpirationResult expireCouponsAsync();
+
+}
